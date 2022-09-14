27ae26c869fc3fd43dae76a47b48acaaa44610cd
Dipali127 <dipalibohara500@gmail.com>
2022-09-13 23:51:01 -0600

    new change

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ app.use(bodyParser.json());
 
 app.use('/', route);
 
+mongoose.connect("mongodb+srv://DipaliBohara:80761668@cluster0.4wyyohq.mongodb.net/dipaliProject2"
+, {
+   useNewUrlParser: true 
+}
+).then( () => {console.log("MongoDb is connected")})
+.catch( err => console.log(err))
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('express app started on the port ' + (process.env.PORT || 3000))
