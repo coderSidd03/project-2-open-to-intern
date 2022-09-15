5305610138270939e9d6640ff318b508705c0fc2
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-09-15 13:12:21 +0530

    validation checked and cahnged in internController.js  api: committed: Soumyadeep Chakraborty

--- src/controllers/collegecontroller.js
+++ src/controllers/collegecontroller.js
@@ -8,21 +8,20 @@ const Validator = require("../validation/validator");
 const CreateCollege = async function (req, res) {
     try {
         let { name, fullName, logoLink, ...rest } = req.body;
-        if (!Validator.checkInputsPresent(req.body)) {
-            return res.status(400).send({ status: false, Error: "please provide details" });
-        }
+
+        if (!Validator.checkInputsPresent(req.body)) return res.status(400).send({ status: false, Error: "please provide details" });
 
         if (Validator.checkInputsPresent(rest)) return res.status(400).send({ status: false, Error: "please provide required details only => name, fullName, logoLink" });
 
         if (!Validator.checkString(name)) return res.status(400).send({ status: false, msg: "name is required" });
         if (!Validator.validateName(name)) return res.status(400).send({ status: false, msg: "name is invalid" });
-        
+
         if (!Validator.checkString(fullName)) return res.status(400).send({ status: false, msg: "fullName is required" });
         if (!Validator.validateName(fullName)) return res.status(400).send({ status: false, msg: "fullName is invalid" });
 
         if (!Validator.checkString(logoLink)) return res.status(400).send({ status: false, msg: "logoLink is required" });
 
-        
+
         const findcollege = await collegeModel.findOne({ name: name });
 
         if (findcollege) return res.status(404).send({ status: false, msg: "college already exist" });
@@ -40,14 +39,15 @@ const CreateCollege = async function (req, res) {
 
 const getCollegeDetails = async function (req, res) {
     try {
-        let collegeName = req.query.collegeName;
+
+        let collegeName = req.query.collegeName.toLowerCase();
 
         if (!collegeName) return res.status(400).send({ status: false, msg: "Provide collegeName in query" });
 
         let getCollegeDetails = await collegeModel.findOne({ name: collegeName, isDeleted: false });
         if (!getCollegeDetails) return res.status(400).send({ status: false, msg: "college not exist or deleted already" });
 
-        let internsDetails = await internModel.find({ collegeId: getCollegeDetails._id, isDeleted: false })
+        let internsDetails = await internModel.find({ collegeId: getCollegeDetails._id, isDeleted: false }).select({ name: 1, email: 1, mobile: 1 })
         if (internsDetails.length == 0) return res.status(400).send({ status: false, msg: "no interns found for the given coollege or deleted already" });
 
 
